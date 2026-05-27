0d5002b22965213b16e7778c8b4aa91d6b231ec8
Muhammad Hassaan Naeem <muhammadhassaannaeem426@gmail.com>
2026-05-27 19:06:18 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="robust_cv_defense",
     version="1.0.0",
-    author="Zarka Yousaf",
+    author="Muhammad Hassaan Naeem",
     description="Adversarial Defense Pipeline with Dual-Metric Evaluation (Classical + ZKP-Informed)",
     python_requires=">=3.10",
     packages=find_packages(where="src"),
